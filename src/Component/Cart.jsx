e8f6e27ac69276a614d3a2b45fcb89bd8c9d71c1
samadhan gore <samadhangore1910@gmail.com>
2024-03-22 23:23:10 +0530

    add cart counting

--- src/Component/Cart.jsx
+++ src/Component/Cart.jsx
@@ -1,10 +1,15 @@
 import React,{useState} from 'react';
 import ItemCard from './ItemCard';
+import { FoodItem } from './Data/FoodItem';
+import { useSelector } from 'react-redux';
+
+
 
 const Cart = () => {
   const[activeCart,setActiveCart] =useState(true);
+  const CartItem=useSelector((store)=>store.cart.items);
 
-  
+
   return (
     <>
       <div className={`fixed right-0 top-0 w-full lg:w-[20vw] h-full p-6 bg-white overflow-y-auto ${activeCart? "translate-x-0":"translate-x-full"} transition-all duration-500 z-10`}>
@@ -12,9 +17,9 @@ const Cart = () => {
           <span className='font-bold text-2xl'>My Order</span>
           <i  onClick={()=>setActiveCart(!activeCart)} className="fa-solid fa-circle-xmark border-2 border-grey-600 text-dark font-bold p-1 text-xl rounded-md hover:bg-red-500 hover:text-white mr-2 mx-3"></i>
         </div>
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
+       
+       
+        <ItemCard  />
 
         <div className='font-bold text-sm p-3 absolute bottom-0 w-full'>
           <h2>Item :</h2>
@@ -24,7 +29,9 @@ const Cart = () => {
         </div>
         
       </div>
-      <i onClick={()=>setActiveCart(!activeCart)} className="fa-solid fa-cart-shopping text-2xl px-3 py-2 rounded-full bg-white text-3xl shadow-md p-3 fixed bottom-4 right-4" ></i>
+      <i onClick={()=>setActiveCart(!activeCart)} className="fa-solid fa-cart-shopping text-2xl px-3 py-2 rounded-full bg-white text-3xl shadow-md p-3 fixed bottom-4 right-4" >
+        <span className='text-sm  radius-[50%] rounded-full right-1 bottom-8 px-2 bg-green-500 text-white absolute  '>{CartItem.length}</span>
+      </i>
     </>
   );
 };
