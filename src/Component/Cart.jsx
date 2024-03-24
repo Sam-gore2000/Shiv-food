42f34882834dd9bd24ec61e28e3f86aee8826eaf
samadhan gore <samadhangore1910@gmail.com>
2024-03-24 21:34:39 +0530

    Add cart increment

--- src/Component/Cart.jsx
+++ src/Component/Cart.jsx
@@ -6,7 +6,7 @@ import { useSelector } from 'react-redux';
 
 
 const Cart = () => {
-  const[activeCart,setActiveCart] =useState(true);
+  const[activeCart,setActiveCart] =useState();
   const CartItem=useSelector((store)=>store.cart.items);
 
 
@@ -15,11 +15,18 @@ const Cart = () => {
       <div className={`fixed right-0 top-0 w-full lg:w-[20vw] h-full p-6 bg-white overflow-y-auto ${activeCart? "translate-x-0":"translate-x-full"} transition-all duration-500 z-10`}>
         <div className='flex justify-between text-center my-4'>
           <span className='font-bold text-2xl'>My Order</span>
-          <i  onClick={()=>setActiveCart(!activeCart)} className="fa-solid fa-circle-xmark border-2 border-grey-600 text-dark font-bold p-1 text-xl rounded-md hover:bg-red-500 hover:text-white mr-2 mx-3"></i>
+          <i  onClick={()=>setActiveCart(!activeCart)} className={"fa-solid fa-circle-xmark ${}border-2 border-grey-600 text-dark font-bold p-1 text-xl rounded-md hover:bg-red-500 hover:text-white mr-2 mx-3"}></i>
         </div>
        
        
-        <ItemCard  />
+     {
+      CartItem.length>=1?CartItem.map((food)=>{
+        return(
+          <ItemCard key={food.id} item={food} id={food.id} name={food.name} img={food.img} price={food.price} qty={food.qty}/>
+        )
+      }):<h2 className='text-xl font-bold text-red-600 text-center'>Your Cart Is Empty</h2>
+     }
+     
 
         <div className='font-bold text-sm p-3 absolute bottom-0 w-full'>
           <h2>Item :</h2>
